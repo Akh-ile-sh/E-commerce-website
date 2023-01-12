d48cdd244ef1636d69cb82ad8fbaf625ffa1aee7
Akhilesh C <akhileshcvdy@gmail.com>
2023-01-12 11:06:09 +0530

    register route updated

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,20 @@
+const User = require("../models/User");
+const StatusCodes = require("http-status-codes");
+const customErrors = require("../errors");
+
 const register = async (req, res) => {
-  res.send("register user");
+  const { email, name, password } = req.body;
+  const emailAlreayExists = await User.findOne({ email });
+
+  if (emailAlreayExists) {
+    throw new customErrors.BadRequestError("Email already exists");
+  }
+
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
   res.send("login user");
